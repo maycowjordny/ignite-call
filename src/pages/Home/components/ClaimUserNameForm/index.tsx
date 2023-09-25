900df8cf6b16e9b78f3aef3bf01b2f8101538b04
Maycow jordny <maycowjordny2015@gmail.com>
2023-09-25 20:17:00 -0300

    finalizando pagina de register e pagina home e iniciando backend com prisma

---
+++ src/pages/Home/components/ClaimUserNameForm/index.tsx
@@ -0,0 +1,55 @@
+import { Button, Text, TextInput } from "@ignite-ui/react";
+import { Form, FormAnnotation } from "./styles";
+import { ArrowRight } from "phosphor-react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useRouter } from "next/router";
+const claimUserNameFormSchema = z.object({
+    username: z.string()
+        .min(3, { message: 'O usuário precisa ter pelo menos 3 letras' })
+        .regex(/^([a-z\\-]+)$/i, { message: 'O usuário pode ter apenas letras e hifens' })
+        .transform(username => username.toLowerCase())
+
+})
+
+type claimUsernameFormData = z.infer<typeof claimUserNameFormSchema>
+
+export function ClaimUserNameForm() {
+
+    const { register, handleSubmit, formState: { errors } } = useForm<claimUsernameFormData>({
+        resolver: zodResolver(claimUserNameFormSchema)
+    })
+
+    const router = useRouter()
+
+    async function handleClaimUsername(data: claimUsernameFormData) {
+        const { username } = data
+
+        await router.push(`/register?username=${username}`)
+    }
+
+    return (
+        <>
+            <Form as="form" onSubmit={handleSubmit(handleClaimUsername)} >
+                <TextInput
+                    size="sm"
+                    prefix="ignite.com/"
+                    placeholder="seu-usuário"
+                    crossOrigin="anonymous"
+                    {...register('username')}
+                />
+                <Button size="sm" type="submit">
+                    Reservar
+                    <ArrowRight />
+                </Button>
+
+            </Form>
+            <FormAnnotation>
+                <Text size='sm'>
+                    {errors.username ? errors.username.message : 'Digite o nome do usuário desejado'}
+                </Text>
+            </FormAnnotation>
+        </>
+    )
+}
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,14 +2,17 @@ import { Heading, Text } from '@ignite-ui/react'
 import { Container, Hero, Preview } from './styles'
 import Image from 'next/image'
 import previewImage from '../../assets/appPreview.png'
+import { ClaimUserNameForm } from './components/ClaimUserNameForm'
 
 export default function Home() {
   return (
     <Container>
       <Hero>
         <Heading size='4xl'>Agendamento descomplicado</Heading>
-        <Text size='lg'>Conecte seu calendário e permita que as pessoas marquem agendamentos
-          no seu tempo livre.</Text>
+        <Text size='xl'>Conecte seu calendário e permita que as pessoas marquem agendamentos
+          no seu tempo livre.
+        </Text>
+        <ClaimUserNameForm />
       </Hero>
       <Preview>
         <Image src={previewImage}
